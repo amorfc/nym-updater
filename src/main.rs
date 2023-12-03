3defc8335d4f7530c306857a6d8c04e750a50913
amorf <fatihermetin@gmail.com>
2023-12-04 01:18:12 +0300

    Refactor main method

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
-use cmd_lib::run_cmd;
-
-use crate::appclient::NymGithubClient;
+use prompt::{AppSelectOption, NymUpdateAssetSelectPrompt};
+use serde::{Deserialize, Serialize};
+use util::{NymAssetUpdateConfig, NymConfigFileUtil};
 
 mod appclient;
+mod constants;
+mod prompt;
+mod util;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let msg = "Hello, world!";
+    let current_config = NymConfigFileUtil::read_config_file()?;
+    let selected_options = current_config.and_then(|config| Some(config.as_app_select_options()));
 
-    let latest_release = NymGithubClient::new().latest_nym_release().await?;
+    let prompt = NymUpdateAssetSelectPrompt::new(selected_options);
+    let prompt_result = prompt.start();
 
-    let res = run_cmd!(echo $msg);
-    dbg!(&latest_release);
+    let temp_tag_name = "0.11.0".to_string();
+
+    if let Some(result) = prompt_result {
+        let config = NymReleaseConfig::from_app_select_options(temp_tag_name, result);
+        NymConfigFileUtil::write_config_file(&config)?;
+    }
 
     Ok(())
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NymReleaseConfig {
+    pub release_tag: String,
+    pub assets: Vec<NymAssetUpdateConfig>,
+}
+
+impl NymReleaseConfig {
+    pub fn from_app_select_options(release_tag: String, assets: Vec<AppSelectOption>) -> Self {
+        NymReleaseConfig {
+            release_tag,
+            assets: assets.into_iter().map(|asset| asset.into()).collect(),
+        }
+    }
+
+    pub fn as_app_select_options(&self) -> Vec<AppSelectOption> {
+        self.assets
+            .iter()
+            .map(|asset| asset.clone().into())
+            .collect()
+    }
 }
