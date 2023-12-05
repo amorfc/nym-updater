52a7533e3b29c159b08a8df35869d91df9cb267b
amorf <fatihermetin@gmail.com>
2023-12-05 08:43:00 +0300

    Test cmd has package command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use cmd_lib::run_fun;
+use cmd::AppCmd;
 use util::NymConfigFileUtil;
 
 mod appclient;
+mod cmd;
 mod constants;
 mod util;
 
@@ -10,6 +11,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let maybe_current_config = NymConfigFileUtil::read_config_file();
 
     let temp_tag_name = "0.11.0".to_string();
+
+    let wget2 = "wget2";
+
+    let result = AppCmd::has_package(wget2);
+
+    dbg!(&result);
 
     Ok(())
 }
