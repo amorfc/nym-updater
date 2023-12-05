0b29e77b5a47b5021e9afa32081227c7f1d69433
amorf <fatihermetin@gmail.com>
2023-12-05 16:25:22 +0300

    Fix config file read mechanism

--- src/util/config_file_util.rs
+++ src/util/config_file_util.rs
@@ -1,41 +1,46 @@
 use std::fs;
 
 use serde::{Deserialize, Serialize};
+use tracing::error;
 
 pub struct NymConfigFileUtil {}
 
 const NYM_CONFIG_FILE_NAME: &str = "auto_update_config.json";
 impl NymConfigFileUtil {
-    pub fn read_config_file() -> Option<NymReleaseConfig> {
-        let config_file = match fs::read_to_string(NYM_CONFIG_FILE_NAME) {
-            Ok(file) => file,
-            Err(e) => {
-                println!(
-                    "Error while reading file {} with {} error",
-                    NYM_CONFIG_FILE_NAME, e
-                );
-                return None;
-            }
-        };
+    pub fn read_config_file() -> Result<NymReleaseConfig, String> {
+        let config_file = fs::read_to_string(NYM_CONFIG_FILE_NAME).map_err(|e| {
+            let err = format!(
+                "Error while reading file {} with {} error",
+                NYM_CONFIG_FILE_NAME, e
+            );
+            error!(err);
+            err
+        })?;
 
-        let current_config = serde_json::from_str::<NymReleaseConfig>(&config_file).ok();
+        let current_config =
+            serde_json::from_str::<NymReleaseConfig>(&config_file).map_err(|e| e.to_string())?;
 
-        current_config
+        Ok(current_config)
     }
 
     pub fn _write_config_file(config: &NymReleaseConfig) -> Result<(), String> {
         let config_file = serde_json::to_string_pretty(&config).map_err(|e| {
-            format!(
+            let err = format!(
                 "Error while serializing config file with {} error",
                 e.to_string()
-            )
+            );
+            error!(err);
+            err
         })?;
 
         fs::write(NYM_CONFIG_FILE_NAME, config_file).map_err(|e| {
-            format!(
+            let err = format!(
                 "Error while writing file {} with {} error",
                 NYM_CONFIG_FILE_NAME, e
-            )
+            );
+
+            error!(err);
+            err
         })?;
 
         Ok(())
@@ -52,5 +57,4 @@ pub struct NymReleaseConfig {
 pub struct NymAssetUpdateConfig {
     pub name: String,
     pub auto_update: bool,
-    pub index: usize,
 }
