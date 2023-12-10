a44cde551aeb15f57cc4623f2ce228cb08419dac
amorf <fatihermetin@gmail.com>
2023-12-10 19:26:31 +0300

    Get ip as string and then add to command

--- src/updater/nym_updater.rs
+++ src/updater/nym_updater.rs
@@ -225,7 +225,10 @@ impl NymUpdater {
         let id = self.node_id().await?;
 
         let ip_command = format!("$(curl ipinfo.io/ip)");
-        let res = run_fun!(sudo $path init --id $id --host $ip_command)
+        let ip = run_fun!(echo $ip_command)
+            .map_err(|e| format!("Error while getting ip address with {} error", e))?;
+        info!("Initing node with id {} and ip {}", id, ip);
+        let res = run_fun!(sudo $path init --id $id --host $ip)
             .map_err(|e| format!("Error while initing mixnode with {} error", e))?;
         info!("Init result: {}", res);
         Ok(())
